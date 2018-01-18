5e6274d49cda0ee140400425060dd21688b45708
Александр Миллин <millin.a@tvzavr.ru>
2018-01-18 11:00:44 +0300

    v1.1.54336

--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@
 # limitations under the License.
 
 # Git version of ClickHouse that we package
-CH_VERSION="${CH_VERSION:-1.1.54327}"
+CH_VERSION="${CH_VERSION:-1.1.54337}"
 
 # Git tag marker (stable/testing)
 CH_TAG="${CH_TAG:-stable}"
