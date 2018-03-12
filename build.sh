e9d125aed48cc051945e6ea951b8d15892474c15
Vladislav Klimenko <sunsingerus@gmail.com>
2018-03-12 16:44:32 +0300

    specifle

--- build.sh
+++ build.sh
@@ -364,7 +364,8 @@ function list_SRPMs()
 function mkdirs()
 {
 	banner "Prepare dirs"
-	mkdir -p "$RPMBUILD_DIR"/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+	mkdir -p "$RPMBUILD_DIR"/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+	mkdir -p "$TMP_DIR"
 }
 
 ##
