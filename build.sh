f45398bb306e805bfac8b59f261e2067af2f2cc1
Vladislav Klimenko <sunsingerus@gmail.com>
2018-11-22 10:44:19 +0300

    v18.14.15-stable

--- build.sh
+++ build.sh
@@ -28,7 +28,7 @@
 # limitations under the License.
 
 # Git version of ClickHouse that we package
-CH_VERSION="${CH_VERSION:-18.14.13}"
+CH_VERSION="${CH_VERSION:-18.14.15}"
 
 # Git tag marker (stable/testing)
 CH_TAG="${CH_TAG:-stable}"
